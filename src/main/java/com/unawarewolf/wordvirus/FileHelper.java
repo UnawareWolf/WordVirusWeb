b44ba9eb6f40677c4b7f7c31c5cac13d74a52dd9
Hal Smith <hal@schnibb.com>
2021-01-05 22:42:19 +0000

    Retry of previous commit.

--- src/main/java/com/unawarewolf/wordvirus/FileHelper.java
+++ src/main/java/com/unawarewolf/wordvirus/FileHelper.java
@@ -1,5 +1,6 @@
 package com.unawarewolf.wordvirus;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
@@ -9,10 +10,7 @@ import java.net.URL;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
@@ -77,53 +75,24 @@ public class FileHelper {
     
     public static String getRandomImagePath() throws IOException, URISyntaxException {
         String imagePath = "";
-//        PathMatchingResourcePatternResolver matchingResolver = new PathMatchingResourcePatternResolver();
+        PathMatchingResourcePatternResolver matchingResolver = new PathMatchingResourcePatternResolver();
         
-//        Resource imagesRoot = matchingResolver.getResource("/static/images");
+        Resource imagesRoot = matchingResolver.getResource("/static/images");
         
 //        String[] imagePaths = imagesRoot.getFile().list();
-//        imagesRoot.getInputStream();
-        List<String> imagePaths = getFilenamesForDirnameFromCP("static/images");
+        InputStream resourceStream = imagesRoot.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(resourceStream));
+        String resource;
+        List<String> imagePaths = new ArrayList<>();
+        while ((resource = br.readLine()) != null) {
+            imagePaths.add(resource);
+        }
         if (imagePaths != null && imagePaths.size() > 0) {
             imagePath = imagePaths.get(new Random().nextInt(imagePaths.size()));
             String[] segments = imagePath.split("\\\\");
             imagePath = segments[segments.length - 1];
         }
         return imagePath;
-    }
-    public static List<String> getFilenamesForDirnameFromCP(String directoryName) throws URISyntaxException, UnsupportedEncodingException, IOException {
-        List<String> filenames = new ArrayList<>();
-
-        URL url = Thread.currentThread().getContextClassLoader().getResource(directoryName);
-        if (url != null) {
-            if (url.getProtocol().equals("file")) {
-                File file = Paths.get(url.toURI()).toFile();
-                if (file != null) {
-                    File[] files = file.listFiles();
-                    if (files != null) {
-                        for (File filename : files) {
-                            filenames.add(filename.toString());
-                        }
-                    }
-                }
-            } else if (url.getProtocol().equals("jar")) {
-                String dirname = directoryName + "/";
-                String path = url.getPath();
-                String jarPath = path.substring(5, path.indexOf("!"));
-                try (JarFile jar = new JarFile(URLDecoder.decode(jarPath, StandardCharsets.UTF_8.name()))) {
-                    Enumeration<JarEntry> entries = jar.entries();
-                    while (entries.hasMoreElements()) {
-                        JarEntry entry = entries.nextElement();
-                        String name = entry.getName();
-                        if (name.startsWith(dirname) && !dirname.equals(name)) {
-                            URL resource = Thread.currentThread().getContextClassLoader().getResource(name);
-                            filenames.add(resource.toString());
-                        }
-                    }
-                }
-            }
-        }
-        return filenames;
     }
     
     public static String getImageInfo(String imagePath) {
